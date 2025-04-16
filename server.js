e429f5ff10019b8234f766cf51501e815f03bd8c
SatyShrey <120662129+SatyShrey@users.noreply.github.com>
2025-04-16 07:24:56 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
 /* eslint-disable no-undef */
 const express = require('express');
-const app = express();
+const cloudinary = require('cloudinary').v2;
+const multer = require('multer');
+const moment=require('moment-timezone')
 const cors = require('cors');
+const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-require('dotenv').config();
+require('dotenv').config(); 
 
-//const bcrypt = require('bcrypt');
+//cloudinary Configuration
+cloudinary.config({
+    cloud_name: 'dptdikuo6',
+    api_key: '161827842144876',
+    api_secret: 'eEjhM3E_kxhlhrLm17GYB5-id8A'
+});
+
+const upload = multer({ dest: './uploads/' });
+
+app.post('/upload', upload.single('image'), (req, res) => {
+    cloudinary.uploader.upload(req.file.path, (error, result) => {
+      if (error) {
+        console.error(error);
+        res.status(500).send({ message: 'Error uploading image' });
+      } else {
+        res.send(result.url);
+      }
+    });
+  });
 
 const mongoClient = require('mongodb').MongoClient;
 const conStr =process.env.PORT || 'mongodb://localhost:27017/'
@@ -17,6 +38,7 @@ const { Server } = require('socket.io');
 const server = http.createServer(app)
 const io = new Server(server, { cors: {} });
 const port = 6060;
+let onlineUsers=[];
 
 //default page
 app.get('/', (req, res) => {
@@ -24,10 +46,18 @@ app.get('/', (req, res) => {
 })
 
 //socket.io
-io.on('connection', (socket) => {
+io.on('connection', async(socket) => {
     const userId = socket.handshake.query.userId;
     socket.join(userId);
-    socket.broadcast.emit('user', userId);
+    let newOnlineUsers=[...onlineUsers,userId]
+    socket.broadcast.emit('online', newOnlineUsers);
+    onlineUsers=newOnlineUsers;
+    
+    //update online status
+    mongoClient.connect(conStr).then(clientObject=>{
+    const db=clientObject.db('hii');
+    db.collection('users').updateOne({ email: userId }, { $set: { lastseen: 'online' } });
+    })
 
     socket.on('chat', (data) => {
         mongoClient.connect(conStr).then(clientObject=>{
@@ -39,6 +69,21 @@ io.on('connection', (socket) => {
             }).catch(e=>{io.to(data.sender).emit('status',e);})
         }).catch(e=>{io.to(data.sender).emit('status',e);})
     });
+
+    //user disconnects
+        socket.on('disconnect', () => {
+            //update online status
+            mongoClient.connect(conStr).then(clientObject=>{
+            const db=clientObject.db('hii');
+            const timestamp =Date.now();
+            const istDateTime=moment(timestamp).tz('Asia/Kolkata').format('YYYY-MM-DD HH:mm:ss')
+            db.collection('users').updateOne({ email: userId }, { $set: { lastseen: istDateTime } }).then(()=>{
+                let newOnlineUsers=onlineUsers.filter(a=>a != userId)
+                socket.broadcast.emit('offline',newOnlineUsers);
+                onlineUsers=newOnlineUsers
+            })
+            })
+        });
 
 
 });
@@ -65,6 +110,13 @@ mongoClient.connect(conStr).then(clientObject => {
     app.get('/users', (req, res) => {
         db.collection('users').find({}).toArray().then(users => {
             res.send(users); res.end();
+        })
+    });
+
+    //get user by id
+    app.get('/user/:id', (req, res) => {
+        db.collection('users').find({email:req.params.id}).then(user => {
+            res.send(user); res.end();
         })
     });
 
