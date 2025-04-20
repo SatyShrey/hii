ff13d9a7b40de0479f4cb4c84ee2615fab10b209
SatyShrey <120662129+SatyShrey@users.noreply.github.com>
2025-04-20 14:42:54 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 const cors = require('cors');
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors());
 
 const cloudinary = require('cloudinary').v2;
 const multer = require('multer');
